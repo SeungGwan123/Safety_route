d85400938d50fcec801a07092e24c59c7810d3c5
kwangyoulsagong <sgky0511@naver.com>
2023-09-17 21:58:04 +0900

    kakao-tmap-naver

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,6 @@
 import Main from "./components/Router/Main";
+import Naver from "./components/Router/Naver"
+import Kakao from "./components/Router/Kakao"
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
@@ -6,6 +8,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="naver" element={<Naver />} />
+        <Route path="/kakao" element={<Kakao />} />
       </Routes>
     </BrowserRouter> 
   );
