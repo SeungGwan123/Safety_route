1ee97108b5315a0cdd566e6f414baaab2e2c8075
kwangyoulsagong <sgky0511@naver.com>
2023-09-18 23:46:48 +0900

    osrm

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,12 @@
 import Main from "./components/Router/Main";
-import Naver from "./components/Router/Naver"
-import Kakao from "./components/Router/Kakao"
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="naver" element={<Naver />} />
-        <Route path="/kakao" element={<Kakao />} />
       </Routes>
     </BrowserRouter> 
   );
--- front/src/components/Router/Main.js
+++ front/src/components/Router/Main.js
@@ -1,108 +1,97 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import { MapContainer, TileLayer, Marker,Popup } from 'react-leaflet';
+import { useEffect } from 'react';
+import "leaflet/dist/leaflet.css"
+import "../styles/style.scss";
 
-/* global Tmapv2 */ // Add this line to inform ESLint about the global variable
 
 function Main() {
-  const [startX, setStartX] = useState('');
-  const [startY, setStartY] = useState('');
-  const [endX, setEndX] = useState('');
-  const [endY, setEndY] = useState('');
-  const [routeCoordinates, setRouteCoordinates] = useState([]);
-  const [map, setMap] = useState(null);
-
   useEffect(() => {
-    // Load Tmap API library
-    const script = document.createElement("script");
-    script.src = "https://openapi.sk.com/tmap/js/v2.0.0/tmap.min.js";
-    script.type = "text/javascript";
-    script.async = true;
-
-    script.onload = () => {
-      // The Tmap API library has loaded, you can now use Tmapv2 here.
-      const map = new Tmapv2.Map("TMapApp", {
-        center: new Tmapv2.LatLng(37.566481622437934, 126.98502302169841),
-        zoom: 15
+    const buttons = document.querySelectorAll('.menu-button');
+    
+    buttons.forEach(button => {
+      button.addEventListener('click', () => {
+        buttons.forEach(b => {
+          b.style.background = "#fff";
+          b.style.color="#000000"
+        });
+        button.style.background= '#258fff';
+        button.style.color="#fff"
       });
-      setMap(map);
-    };
-
-    document.head.appendChild(script);
-
-    // Unload the script when the component unmounts
+    });
+    
     return () => {
-      document.head.removeChild(script);
-    }
-  }, []);
-  
-  const handleCalculateRoute = () => {
-    axios
-      .get('/calculate-route', {
-        params: {
-          startX,
-          startY,
-          endX,
-          endY,
-        },
-      })
-      .then((response) => {
-        const coordinates = response.data.features[0].geometry.coordinates;
-        setRouteCoordinates(coordinates);
-
-        // Draw the route manually on the map
-        if (map) {
-          const routePath = new Tmapv2.Polyline({
-            map,
-            path: coordinates.map((coord) => new Tmapv2.LatLng(coord[1], coord[0])),
-            strokeColor: "#FF0000",
-            strokeWeight: 5,
+      buttons.forEach(button => {
+        button.removeEventListener('click', () => {
+          buttons.forEach(b => {
+            b.style.background = "#fff";
+            b.style.color="#000000"
           });
-        }
-      })
-      .catch((error) => {
-        console.error(error);
+          button.style.background= '#258fff';
+          button.style.color="#fff"
+        });
       });
-  };
+    };
+  }, []);
+  function Route(){
+    const address= document.querySelector(".input-address")
+    const searchIcon=document.querySelector(".Search")
+    const start=document.querySelector(".start")
+    const end=document.querySelector(".end")
+    const route=document.querySelector(".route-button")
+    address.style.visibility="hidden"
+    searchIcon.style.visibility="hidden"
+    start.style.visibility="visible"
+    end.style.visibility="visible"
+    route.style.visibility="visible"
 
+  }
   return (
-    <div className="main">
-      <div
-        id="TMapApp"
-        style={{
-          height: '50%', // Adjust the height as needed
-          width: '100%',
-          position: 'fixed',
-        }}
-      />
-      <div>
-        <input
+    <div className='main'>
+          <MapContainer center={[37.5665, 126.9780]} zoom={15} scrollWheelZoom={true} style={{ width: "100%", height: "100vh" }}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[37.5665, 126.9780]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
+<div className='menu-bar'>
+  <div className='logo'><li>로고</li></div>
+  <div className='menu-button' style={{background:"#258fff", color:"#fff"}}><li>검색</li></div>
+  <div className='menu-button' onClick={Route}><li>길찾기</li></div>
+  <div className='menu-button'><li>CCTV</li></div>
+  
+ 
+</div>
+<div className="menu">
+        <div className='nav'>
+        <button className='route'>도보경로</button>
+        <button className='Safety-route'>안심도보경로</button>
+        <input className='input-address'
           type="text"
-          placeholder="출발지 경도"
-          value={startX}
-          onChange={(e) => setStartX(e.target.value)}
+          placeholder="주소를 입력하세요"
         />
-        <input
+        <div className='Search' ></div>
+        <input className='start'
           type="text"
-          placeholder="출발지 위도"
-          value={startY}
-          onChange={(e) => setStartY(e.target.value)}
+          placeholder="출발지"
         />
-        <input
+        <input className='end'
           type="text"
-          placeholder="도착지 경도"
-          value={endX}
-          onChange={(e) => setEndX(e.target.value)}
+          placeholder="도착지"
         />
-        <input
-          type="text"
-          placeholder="도착지 위도"
-          value={endY}
-          onChange={(e) => setEndY(e.target.value)}
-        />
-        <button onClick={handleCalculateRoute}>경로찾기</button>
+        <button className='route-button' >경로 검색</button>
+        </div>
+        
       </div>
     </div>
-  );
+
+  )
+  
 }
 
 export default Main;
