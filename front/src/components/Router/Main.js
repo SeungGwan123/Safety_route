30d8789dd97b9ef1869f7abf690a210bcf224a19
kwangyoulsagong <sgky0511@naver.com>
2023-10-05 01:03:50 +0900

    search

--- front/src/components/Router/Main.js
+++ front/src/components/Router/Main.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef,useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import L from "leaflet";
 import { Link } from 'react-router-dom';
@@ -7,36 +7,35 @@ import "../styles/style.scss";
 import axios from "axios";
 
 function Main() {
-  // axios.get("http://127.0.0.1:5000/route/v1/driving/127.092088,37.335339;127.085124,37.337822?steps=true").then(function (res){
-  //   console.log(res.data)
-  // })
-  
-
-  const addressCoordinates = {
-    '아카데미로 119': { lat: 37.375924147295455, lng: 126.63285162967031 }, // Replace with your own dataset
-    '송도 타임스페이스': { lat: 37.38308798021501, lng: 126.64251548843 },
-    '서울':{ lat: 37.5665, lng: 126.9780 }
-    // Add more addresses and coordinates as needed
-  };
+  const Nominatim_Base_Url = "https://nominatim.openstreetmap.org/search";
 
   const [address, setAddress] = useState("");
   const [markerPosition, setMarkerPosition] = useState([37.5665, 126.9780]);
   const mapRef = useRef();
 
-  const searchAddress = () => {
-    const coordinates = addressCoordinates[address];
+  const searchAddress = async () => {
+    try {
+      const response = await axios.get(Nominatim_Base_Url, {
+        params: {
+          q: address,
+          format: 'json',
+          addressdetails: '1',
+        },
+      });
 
-    if (coordinates) {
-      const { lat, lng } = coordinates;
-      if (mapRef.current) {
-        // Set the view of the TileLayer (OSM) to the new coordinates
-        mapRef.current.setView([lat, lng], 15);
-        setMarkerPosition([lat, lng]);
+      if (response.data && response.data.length > 0) {
+        const { lat, lon } = response.data[0];
+        if (mapRef.current) {
+          mapRef.current.setView([lat, lon], 15);
+          setMarkerPosition([lat, lon]);
+        }
+      } else {
+        console.error("Address not found.");
       }
-    } else {
-      console.error("Address not found in the dataset.");
+    } catch (error) {
+      console.error("Error searching for address:", error);
     }
-  }
+  };
 
   const customIcon = new L.Icon({
     iconUrl: require("../img/search.png"), // 이미지 경로를 올바르게 지정하세요.
@@ -54,17 +53,17 @@ function Main() {
         />
         <Marker position={markerPosition} icon={customIcon}>
           <Popup>
-            여기는 서울입니다 <br /> 위도:37.5665 경도:126.9780 <br/>도로명: 서울 중구 세종대로 110 서울특별시청<br/>
-            지번: 태평로1가 31<br/>
+            여기는 서울입니다 <br /> 위도: 37.5665 경도: 126.9780 <br />도로명: 서울 중구 세종대로 110 서울특별시청<br />
+            지번: 태평로1가 31<br />
             우편번호: 04524
-          </Popup>  
+          </Popup>
         </Marker>
       </MapContainer>
       <div className='menu-bar'>
-      <Link className='logo'>로고</Link>
-      <Link className='menu-button' style={{background:"#258fff", color:"#fff"}} to="/">검색</Link>
-      <Link className='menu-button' to="/direction">길찾기</Link>
-      <Link className='menu-button' to="/cctv">CCTV</Link>
+        <Link className='logo'>로고</Link>
+        <Link className='menu-button' style={{ background: "#258fff", color: "#fff" }} to="/">검색</Link>
+        <Link className='menu-button' to="/direction">길찾기</Link>
+        <Link className='menu-button' to="/cctv">CCTV</Link>
       </div>
       <div className="menu">
         <div className='nav'>
@@ -74,6 +73,11 @@ function Main() {
             placeholder="주소를 입력하세요!"
             value={address}
             onChange={(e) => setAddress(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                searchAddress();
+              }
+            }}
           />
           <div className='Search' onClick={searchAddress}></div>
         </div>
